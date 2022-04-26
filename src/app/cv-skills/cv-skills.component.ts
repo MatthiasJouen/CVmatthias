e7d12b97767bb6f788eeebec525cc6dc959063da
MatthiasJouen <matthias.jouen@edu.esiee.fr>
2022-04-26 14:20:00 +0200

    Skills part

--- src/app/cv-skills/cv-skills.component.ts
+++ src/app/cv-skills/cv-skills.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cv-skills.component.scss']
 })
 export class CvSkillsComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
