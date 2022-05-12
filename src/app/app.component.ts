bbdc2072e11bedd8da8b636f0f23570963890515
MatthiasJouen <matthias.jouen@edu.esiee.fr>
2022-05-12 23:23:12 +0200

    change favicon and correct some errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -6,8 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CVmatthias';
+  
+  ngOnInit(): void {
+
+    //Gestion d'une clé localStorage pour stocker les données de l'utilisateur
+    const localStorageTest = 'LocalStorageTest';
+    if(localStorage.getItem('LocalStorageTest') === null) {
+      localStorage.setItem(localStorageTest, 'neverClicktoTop');
+    }
+
+  }
+  
 }
 
 
